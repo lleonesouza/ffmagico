69775f453966373e2045d415b2fbbaa67c440782
lleonesouza <aliencode.host@gmail.com>
2019-10-24 13:55:02 -0300

    Updates

---
+++ precache-manifest.180f32a26655f78f89522316baaf7b73.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "a26b949108fef04326ba",
+    "url": "/ffmagico/static/css/main.75fd1f72.chunk.css"
+  },
+  {
+    "revision": "a26b949108fef04326ba",
+    "url": "/ffmagico/static/js/main.a26b9491.chunk.js"
+  },
+  {
+    "revision": "2aa49b5097daf808fdf1",
+    "url": "/ffmagico/static/js/1.2aa49b50.chunk.js"
+  },
+  {
+    "revision": "c5706937b7336359b230",
+    "url": "/ffmagico/static/js/runtime~main.c5706937.js"
+  },
+  {
+    "revision": "78a0c7d68ad78625095fce39d4f3c08e",
+    "url": "/ffmagico/static/media/video.78a0c7d6.mp4"
+  },
+  {
+    "revision": "9807ed8c1640c68ec3435194565e1bff",
+    "url": "/ffmagico/static/media/video02.9807ed8c.mp4"
+  },
+  {
+    "revision": "0dcb90e2e1d08a5d0e31c0a85aa75896",
+    "url": "/ffmagico/static/media/logo.0dcb90e2.svg"
+  },
+  {
+    "revision": "89a8d239686382aa95c5040bb483e8ee",
+    "url": "/ffmagico/static/media/Atifa.89a8d239.otf"
+  },
+  {
+    "revision": "5942b47b2a882dcf83f70c88834275a0",
+    "url": "/ffmagico/static/media/Ubuntu-Medium.5942b47b.ttf"
+  },
+  {
+    "revision": "1516b145fe4182bb0da105070694f82f",
+    "url": "/ffmagico/static/media/Laila-Light.1516b145.ttf"
+  },
+  {
+    "revision": "48adddeaff3bb01828f8c1d7a943031d",
+    "url": "/ffmagico/static/media/Mukta-Bold.48adddea.ttf"
+  },
+  {
+    "revision": "b21dea83e5ffde2516d184a239eed9df",
+    "url": "/ffmagico/static/media/ffbaliga.b21dea83.jpg"
+  },
+  {
+    "revision": "28b50e74e3d53715fc3a41bb6d25b4c3",
+    "url": "/ffmagico/static/media/fire.28b50e74.mov"
+  },
+  {
+    "revision": "4973121bd694d340a04ed20b2e686b04",
+    "url": "/ffmagico/static/media/Royal.4973121b.ttf"
+  },
+  {
+    "revision": "e631bbc7cd25aab6580f22af8a58dc7d",
+    "url": "/ffmagico/static/media/Faith Collapsing.e631bbc7.ttf"
+  },
+  {
+    "revision": "c2b9bd1af7e8f401fc78cb07ed7d296d",
+    "url": "/ffmagico/static/media/cara.c2b9bd1a.jpg"
+  },
+  {
+    "revision": "a915e00f53a30e9bfc79e3a276e6ccb1",
+    "url": "/ffmagico/static/media/corporativo_new.a915e00f.jpg"
+  },
+  {
+    "revision": "851d6dc21442067908b4a7c9ae332460",
+    "url": "/ffmagico/static/media/eventos.851d6dc2.jpg"
+  },
+  {
+    "revision": "e8eba6f3c6148a9699b9c3350b96599b",
+    "url": "/ffmagico/static/media/particulares2.e8eba6f3.jpg"
+  },
+  {
+    "revision": "598af706908ac6beafedf648e2c34240",
+    "url": "/ffmagico/index.html"
+  }
+];
